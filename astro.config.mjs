b57e3a5eece5639e9f357d099b56d3686837cb05
Santiago Rodriguez <>
2024-04-17 22:54:51 -0500

    set gh config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://santrodriguez-dev.github.io/minimalist-portfolio-json.github.io/',
+  site: 'https://santrodriguez-dev.github.io',
+  base: '/minimalist-portfolio-json',
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
