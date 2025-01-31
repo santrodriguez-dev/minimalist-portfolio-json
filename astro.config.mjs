d1326370ca86df13df5ed2f337de938c8c4e04d9
Santiago Rodriguez <>
2025-01-31 11:29:07 -0500

    update dependencies and improve CV details

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://santrodriguez-dev.github.io',
-  base: "minimalist-portfolio-json",
+  base: "/minimalist-portfolio-json",
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
   },
   redirects: {
-    "/es": "./",
+    "/es": "/",
   }
 })
